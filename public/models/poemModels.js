a837be0421d6068e4bde456e82cff4e81284b1cf
simonzsolt <simonzsoltjota@gmail.com>
2015-08-16 23:08:31 +0200

    reset env var

--- public/models/poemModels.js
+++ public/models/poemModels.js
@@ -1,10 +1,10 @@
 var mongoose = require('mongoose'),
     autoIncrement = require('mongoose-auto-increment'),
-    Schema = mongoose.Schema,
-    config = require('../../config.json')[env];
+    Schema = mongoose.Schema;
 
 // session storage connection
-var connection = mongoose.createConnection(config.mongoDb, function(err) {
+var connection = mongoose.createConnection(
+    process.env.OPENSHIFT_MONGODB_DB_URL, function(err) {
 
     if (err) {
         console.log('DB connection error:' + err);
