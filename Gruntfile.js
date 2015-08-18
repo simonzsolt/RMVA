16850103624eaffeff25414edfb4fe3fc7fcef09
simonzsolt <simonzsoltjota@gmail.com>
2015-08-18 22:22:37 +0200

    grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -247,8 +247,8 @@ module.exports = function(grunt) {
     });
 
     // Default tasks
-    grunt.registerTask('default',   ['devUpdate', 'githooks']);
-    grunt.registerTask('dev',       ['devUpdate', 'wiredep', 'open:dev', 'githooks']);
+    grunt.registerTask('default',   ['devUpdate']);
+    grunt.registerTask('dev',       ['devUpdate', 'wiredep', 'open:dev']);
     grunt.registerTask('build',     
         [
             'devUpdate', 
@@ -256,9 +256,9 @@ module.exports = function(grunt) {
             'concat', 
             'uglify', 
             'cssmin',
-            'htmlmin',
-            'githooks'
+            'htmlmin'
         ]
     );
     grunt.registerTask('changes',   ['watch']);  
+    grunt.registerTask('hook',   ['githooks']);  
 };
