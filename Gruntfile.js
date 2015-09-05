4fbcbd3ffce0cf6fccef0c96091d568075bc6871
simonzsolt <simonzsoltjota@gmail.com>
2015-09-05 17:01:49 +0200

    fix typo

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,8 +6,8 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-ng-annotate');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-cs.);
-    grunt.loadNpmTasks('grunt-contrib-htm.);
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-githooks');
     grunt.loadNpmTasks('grunt-supervisor');
 
